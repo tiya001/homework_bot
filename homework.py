842df648d0da2c65122ef14ba3bc10609bf6b313
Якупов Тимур <tiy001@yandex.ru>
2022-11-28 22:09:37 +0300

    7 sprint

--- homework.py
+++ homework.py
@@ -31,8 +31,8 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
