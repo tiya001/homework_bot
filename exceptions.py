314e68a25b4b7a5c2d41db6e9c83ff13f1214239
Якупов Тимур <tiy001@yandex.ru>
2022-11-28 22:23:09 +0300

    sad

--- exceptions.py
+++ exceptions.py
@@ -1,23 +1,23 @@
-class BaseError(Exception):
+class Error(Exception):
     """Базовый класс ошибок."""
     pass
 
 
-class SendMessageError(BaseError):
+class SendMessageError(Error):
     """Не удалось отправить сообщение."""
     pass
 
 
-class UnexpectedStatusCodeError(BaseError):
+class UnexpectedStatusCodeError(Error):
     """Ответ API содержит статус код, отличный от 200."""
     pass
 
 
-class ExpectedKeysNotFoundError(BaseError):
+class ExpectedKeysNotFoundError(Error):
     """Отсутствие ожидаемых ключей в ответе API."""
     pass
 
 
-class UnexpectedStatusError(BaseError):
+class UnexpectedStatusError(Error):
     """Обнаружен недокументированный статус домашней работы в ответе API."""
     pass
